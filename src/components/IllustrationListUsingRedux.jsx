dd899042a18f68298786d993e73963c027dbf623
rajkumar <rema1191@gmail.com>
2026-01-30 20:01:22 +0530

    axios

--- src/components/IllustrationListUsingRedux.jsx
+++ src/components/IllustrationListUsingRedux.jsx
@@ -2,13 +2,35 @@ import Illustration from "./Illustration";
 import "./IllustrationList.css";
 import { IllustrationContext } from "../context/IllustrationContext";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const IllustrationListUsingRedux = () => {
+  const [illusData, setIllusData] = useState([]);
   const illustrations = useSelector(
     (state) => state.illustrations.illustrations,
   );
 
-  console.log(illustrations);
+  useEffect(() => {
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     // Dispatch an action to update the Redux store with fetched data
+    //     // For example: dispatch({ type: 'SET_ILLUSTRATIONS', payload: data });
+    //     setIllusData(data);
+    //   }
+    //   );
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        setIllusData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  console.log(illusData);
 
   const filteredList = illustrations.filter((illus) => illus.year >= 2024);
 
